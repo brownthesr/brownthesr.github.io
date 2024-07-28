0773c664cba8fabc425e0edc3278358b08fec99f
Drake Brown <brownthesr@gmail.com>
2024-07-27 18:19:09 -0600

    added comments to the JS in charge of images

--- js/939898b427480d700449229ff00dbb8a6f9f77442b532f697866e6914ab8843a.js
+++ js/939898b427480d700449229ff00dbb8a6f9f77442b532f697866e6914ab8843a.js
@@ -1,13 +1,27 @@
-! function() {
+(function() {
     "use strict";
-    const e = Array.from(document.getElementsByTagName("img")),
-        t = e => {
-            const t = e.parentElement;
-            t && (t.style.animation = "none")
-        };
-    e.forEach((e => {
-        e.complete ? t(e) : e.addEventListener("load", (() => {
-            t(e)
-        }))
-    }))
-}();
+
+    // Get all image elements on the page
+    const images = Array.from(document.getElementsByTagName("img"));
+
+    // Function to stop animation on the parent of a given image
+    const stopParentAnimation = (img) => {
+        const parent = img.parentElement;
+        if (parent) {
+            parent.style.animation = "none";
+        }
+    };
+
+    // Loop through each image element
+    images.forEach((img) => {
+        if (img.complete) {
+            // If the image is already loaded, stop the parent's animation
+            stopParentAnimation(img);
+        } else {
+            // Otherwise, add a load event listener to stop the animation once loaded
+            img.addEventListener("load", () => {
+                stopParentAnimation(img);
+            });
+        }
+    });
+})();
